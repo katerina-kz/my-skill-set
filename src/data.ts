096205b94f336402ec469cff0963d872fade0cfc
katerina-kz <kzvolinska@ebsco.com>
2023-11-02 14:25:55 +0200

    add new branch

---
+++ src/data.ts
@@ -0,0 +1,331 @@
+export default {
+    name: 'Skills',
+    data: {
+        labels: [
+            '2019-04.01',
+            '2020-06-01',
+            '2021-10-01',
+            '2022-06-01',
+            'Current time'
+        ],
+        datasets: [
+            {
+                label: 'HTML/CSS',
+                type: 'line',
+                fill: false,
+                data: [
+                    45, 50, 77, 80, 80
+                ],
+                borderColor: '#04AA6D',
+                backgroundColor: '#04AA6D'
+            },
+            {
+                label: 'JavaScript',
+                type: 'line',
+                fill: false,
+                data: [
+                    10, 35, 53, 73, 80
+                ],
+                borderColor: 'red',
+                backgroundColor: 'red'
+            },
+            {
+                label: 'React',
+                type: 'line',
+                fill: false,
+                data: [
+                    0, 48, 60, 76, 83
+                ],
+                borderColor: 'rgb(97 218 251)',
+                backgroundColor: 'rgb(97 218 251)'
+            },
+            {
+                label: 'Redux',
+                type: 'line',
+                fill: false,
+                data: [
+                    0, 35, 45, 55, 60
+                ],
+                borderColor: '#764ABF',
+                backgroundColor: '#764ABF'
+            },
+            {
+                label: 'TypeScript',
+                type: 'line',
+                fill: false,
+                data: [
+                    0, 0, 0, 68, 80
+                ],
+                borderColor: '#3178c6',
+                backgroundColor: '#3178c6'
+            },
+            {
+                label: 'Node.js',
+                type: 'line',
+                fill: false,
+                data: [
+                    0, 0, 15, 35, 56
+                ],
+                borderColor: '#84ba64',
+                backgroundColor: '#84ba64'
+            },
+            {
+                label: 'Express',
+                type: 'line',
+                fill: false,
+                data: [
+                    0, 0, 15, 20, 30
+                ],
+                borderColor: '#555',
+                backgroundColor: '#555'
+            },
+            {
+                label: 'React Router',
+                type: 'line',
+                fill: false,
+                data: [
+                    0, 0, 14, 35, 58
+                ],
+                borderColor: '#1e1e1e',
+                backgroundColor: '#1e1e1e'
+            },
+            {
+                label: 'Webpack',
+                type: 'line',
+                fill: false,
+                data: [
+                    0, 0, 18, 24, 38
+                ],
+                borderColor: '#8dd6f980',
+                backgroundColor: '#8dd6f980'
+            },
+            {
+                label: 'Gulp',
+                type: 'line',
+                fill: false,
+                data: [
+                    0, 25, 25, 25, 25
+                ],
+                borderColor: '#cf4647',
+                backgroundColor: '#cf4647'
+            },
+            {
+                label: 'Sass',
+                type: 'line',
+                fill: false,
+                data: [
+                    0, 0, 60, 65, 67,
+                ],
+                borderColor: '#c54786',
+                backgroundColor: '#c54786'
+            },
+            {
+                label: 'ThreeJS',
+                type: 'line',
+                fill: false,
+                data: [
+                    0, 0, 70, 45, 37,
+                ],
+                borderColor: '#ceaa19',
+                backgroundColor: '#ceaa19'
+            },
+            {
+                label: 'Jest',
+                type: 'line',
+                fill: false,
+                data: [
+                    0, 0, 52, 65, 65,
+                ],
+                borderColor: '#942d60',
+                backgroundColor: '#942d60'
+            },
+            {
+                label: 'Enzyme',
+                type: 'line',
+                fill: false,
+                data: [
+                    0, 0, 20, 35, 47,
+                ],
+                borderColor: 'blue',
+                backgroundColor: 'blue'
+            },
+            {
+                label: 'React Testing Library',
+                type: 'line',
+                fill: false,
+                data: [
+                    0, 0, 35, 75, 80
+                ],
+                borderColor: '#942d60',
+                backgroundColor: '#942d60'
+            },
+            {
+                label: 'Playwright',
+                type: 'line',
+                fill: false,
+                data: [
+                    0, 0, 0, 54, 63
+                ],
+                borderColor: '#e81811',
+                backgroundColor: '#e81811'
+            },
+            {
+                label: 'Nightwatch',
+                type: 'line',
+                fill: false,
+                data: [
+                    0, 0, 12, 40, 40
+                ],
+                borderColor: '#257d0d',
+                backgroundColor: '#257d0d'
+            },
+            {
+                label: 'MongoDB/Mongoose',
+                type: 'line',
+                fill: false,
+                data: [
+                    0, 0, 0, 0, 35,
+                ],
+                borderColor: '#00684a',
+                backgroundColor: '#00684a'
+            },
+            {
+                label: 'NPM/Yarn',
+                type: 'line',
+                fill: false,
+                data: [
+                    0, 0, 30, 45, 56
+                ],
+                borderColor: '#a32323',
+                backgroundColor: '#a32323'
+            },
+            {
+                label: 'Jira',
+                type: 'line',
+                fill: false,
+                data: [
+                    0, 15, 30, 45, 55
+                ],
+                borderColor: '#0052cc',
+                backgroundColor: '#0052cc'
+            },
+            {
+                label: 'ESLint',
+                type: 'line',
+                fill: false,
+                data: [
+                    0, 20, 36, 41, 49
+                ],
+                borderColor: '#4B32C3',
+                backgroundColor: '#4B32C3'
+            },
+            {
+                label: 'Babel',
+                type: 'line',
+                fill: false,
+                data: [
+                    0, 0, 0, 35, 54
+                ],
+                borderColor: 'yellow',
+                backgroundColor: 'yellow'
+            },
+            {
+                label: 'Git/GitHub/GitLab',
+                type: 'line',
+                fill: false,
+                data: [
+                    0, 50, 65, 70, 80
+                ],
+                borderColor: '#f14e32',
+                backgroundColor: '#f14e32'
+            },
+            {
+                label: 'Docker',
+                type: 'line',
+                fill: false,
+                data: [
+                    0, 0, 0, 0, 45
+                ],
+                borderColor: '#003f8c',
+                backgroundColor: '#FFFFFF'
+            },
+            {
+                label: 'AWS Clouds',
+                type: 'line',
+                fill: false,
+                data: [
+                    0, 0, 0, 0, 45
+                ],
+                borderColor: '#ec7211',
+                backgroundColor: '#ec7211'
+            },
+            {
+                label: 'Date-fns',
+                type: 'line',
+                fill: false,
+                data: [
+                    0, 0, 0, 0, 60
+                ],
+                borderColor: '#ac080d',
+                backgroundColor: '#ac080d'
+            },
+            {
+                label: 'Next.JS',
+                type: 'line',
+                fill: false,
+                data: [
+                    0, 0, 0, 15, 25
+                ],
+                borderColor: '#08ac86',
+                backgroundColor: '#08ac86'
+            },
+            {
+                label: 'CI/CD',
+                type: 'line',
+                fill: false,
+                data: [
+                    0, 0, 20, 28, 49
+                ],
+                borderColor: '#5208ac',
+                backgroundColor: '#5208ac'
+            },
+        ]
+    },
+    options: {
+        plugins: {
+            annotation: {
+                annotations: {}
+            },
+            legend: {
+                position: 'bottom',
+                labels: {
+                    padding: 50,
+                    usePointStyle: true
+                }
+            }
+        },
+        layout: {
+            paddingBottom: 50
+        },
+        scales: {
+            x: {
+                ticks: {
+                    beginAtZero: true
+                }
+            },
+            y: {
+                ticks: {
+                    beginAtZero: true,
+                    stepSize: 25,
+                    callback: (value: any) => {
+                        if (value === 0) return ``;
+                        if (value < 26) return `Novice`;
+                        if (value < 51) return `Intermediate`;
+                        if (value < 77) return `Advanced`;
+                        if (value < 101) return `Expert`;
+                    }
+                }
+            }
+        }
+    }
+};
